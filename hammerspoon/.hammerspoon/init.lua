81b07bcd0336042dda13fd4a15a6af2707990917
Pablo Cobelli <pablocobelli@gmail.com>
2016-07-03 19:02:35 -0300

    Added kwm config.

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -2,6 +2,10 @@
 
 local appfinder = require "hs.appfinder"
 local hints = require "hs.hints"
+local window = require "hs.window"
+local application = require "hs.application"
+local tabs = require "hs.tabs"
+require "tprint"
 
 -- Reload configuration using CMD+ALT+CTRL+R.
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "R", function()
@@ -16,4 +20,24 @@ hs.hints.showTitleThresh = 0
 hs.hints.hintChars = {'F', 'J', 'D', 'S', 'T', 'U'}
 hs.hotkey.bind({"cmd"},"k", function() hints.windowHints(appfinder.appFromName("iTerm2"):allWindows()) end)
 
+-- Ideas:
+-- 1) bring all python figure windows to front
+-- 2) toggle safari to front or back (for quick search for help)
+--
+
+-- Activates python figure windows
+hs.hotkey.bind({"cmd"}, "U", function()
+  pythonapp = application.get'python'
+  -- print(pythonapp:title())
+  pythonapp:unhide()
+  pythonapp:activate(1)
+end)
+
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "H", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+
+  f.x = f.x - 10
+  win:setFrame(f)
+end)
 
